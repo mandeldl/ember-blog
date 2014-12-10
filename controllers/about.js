f18a7844c04615ed5c25424bdc3b23d427de52f2
David Mandel <MandelDL@gmail.com>
2014-12-10 00:10:16 -0500

    Added controllers to about and contact

---
+++ controllers/about.js
@@ -0,0 +1,7 @@
+Blogger.AboutController = Ember.Controller.extend({
+	actions: {
+		showRealName: function  () {
+			alert('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, nulla.');
+		}
+	}
+});
